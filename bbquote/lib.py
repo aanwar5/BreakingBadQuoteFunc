f5fa33bfe2de650890d439fff0bc1cf8a0b03134
Abdullah Anwar <abdullahanwar7@gmail.com>
2021-06-29 19:31:26 +1000

    Movie Quote maker haah

--- bbquote/lib.py
+++ bbquote/lib.py
@@ -1,11 +1,11 @@
 import requests
 
 def get_quote():
-    url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    url = 'https://movie-quote-api.herokuapp.com/v1/quote/'
 
-    response = requests.get(url).json()[0]
+    response = requests.get(url).json()
 
-    return f"{response['quote']}   -{response['author']}"
+    return f"{response['quote']}   -{response['show']} - {response['role']}"
 
 if __name__ == "__main__":
     print(get_quote())
